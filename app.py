63f95a5ee04cf7629addeda81e9abcc5295274a5
Marcel <marcel.mulders@hotmail.com>
2021-08-03 15:23:17 +0100

    changes to app overall

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
+# Maintained
 import env
 from ssh import SSHHandler
+
+# Installed
 from flask import Flask
 from flask import render_template
 import os
@@ -7,20 +10,18 @@ import json
 import time
 import multiprocessing as mp
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
-###########################################################
-# ROUTES
-###########################################################
+# ###########################################################
+# # ROUTES
+# ###########################################################
 
-@app.route("/", methods=["GET"])
-def index():
-    contents = {
-        "page_title": "Application Settings Page",
-    }
-    return render_template('index.html', contents=contents)
-
-
+# @app.route("/", methods=["GET"])
+# def index():
+#     contents = {
+#         "page_title": "Application Settings Page",
+#     }
+#     return render_template('index.html', contents=contents)
 
 
 
@@ -34,23 +35,78 @@ def ssh_app(project_root):
     ss._client()
     # Sync loop
     while os.environ["RUN_SSH_APP"] == "TRUE":
+        # Set application run variables
         print("Reading 'settings.json'...")
         with open(os.path.join(project_root, "settings.json"), "r") as file:
             file_contents = json.loads(file.read())
         os.environ["RUN_SSH_APP"] = "TRUE" if file_contents["RUN_SSH_APP"] else "FALSE"
-        print(
-            "Environment VAR RUN_SSH_APP is: {}".format(
-                os.environ["RUN_SSH_APP"]
-            )
-        )
+        command = "du"
+        if file_contents["REMOTE_COMMAND"]:
+            command = file_contents["REMOTE_COMMAND"]
+
+        # Get remote file sizes
         response = ss._exec(
-            "du {}".format(
+            "{} {}".format(
+                command,
                 os.environ.get("REMOTE_HOST_FOLDER")
             )
         )
-        print(json.dumps(response, indent=4))
+        # print(json.dumps(response, indent=4))
+
+        stdout = response.get("stdout")
+        if not stdout:
+            continue
+        
+        # Get remote folder size on linux
+        print("Calculating REMOTE_HOST_FOLDER size...")
+        remote_base_folder_size = 0
+        for line in stdout:
+            size, folder_path = line.replace("\n", "").split("\t")
+            if folder_path == os.environ.get("REMOTE_HOST_FOLDER"):
+                remote_base_folder_size = size
+        
+        if remote_base_folder_size == 0:
+            continue
+
+        # Get local folder size, any
+        print("Calculating LOCAL_HOST_FOLDER size...")
+        local_base_folder_size = 0
+        for path, dirs, files in os.walk(os.environ.get("LOCAL_HOST_FOLDER")):
+            file_contents["LOCAL_FOLDER"][path] = 0
+            for file in files:
+                fp = os.path.join(path, file)
+                local_base_folder_size += os.path.getsize(fp)
+
+        if local_base_folder_size == 0:
+            continue
+        
+        print(remote_base_folder_size)
+        print(local_base_folder_size)
+        if remote_base_folder_size != local_base_folder_size:
+            print("Local folder out of sync with remote folder")
+            # Remote remote folder
+            # response = ss._exec(
+            #     "rm -rf {}".format(
+            #         os.environ.get("REMOTE_HOST_FOLDER")
+            #     )
+            # )
+            # # Send folder over
+            # ss._put_file(
+            #     src=os.environ.get("LOCAL_HOST_FOLDER"),
+            #     dest=os.environ.get("REMOTE_HOST_FOLDER")
+            # )
+
+        with open(os.path.join(project_root, "settings.json"), "r") as file:
+            file.write(
+                json.dumps(
+                    file_contents
+                )
+            )
+
+        # Sync sleep
         time.sleep(10)
 
+    # Bring down application
     print("Gracefully bringing down ssh_app()")
     folder_contents = os.listdir(project_root)
     files_to_remove = []
@@ -66,46 +122,52 @@ def ssh_app(project_root):
                 file
             )
         )
-    print("Terminating ssh_app...")
+    print("Terminating ssh_app()...")
 
 
-if __name__ == "__main__":
-
+def main_ssh():
     print("Starting application...")
     ssh_process = None
     run_ssh_process = True
     project_root = os.getcwd()
 
     # Initialise the settings.json file with run = True
-    with open(os.path.join(project_root, "settings.json"), "w") as file:
-        file.write(
-            json.dumps({})
-        )
     with open(os.path.join(project_root, "settings.json"), "r") as file:
         file_contents = json.loads(file.read())
-        print(file_contents)
-    file_contents["RUN_SSH_APP"] = True
-    os.environ["RUN_SSH_APP"] = "TRUE"
+    if not file_contents["RUN_SSH_APP"]:
+        print("Application instance already exists...")
+        return None
+    os.environ["RUN_SSH_APP"] = "TRUE" if file_contents["RUN_SSH_APP"] else "FALSE"
+    
+    # Default variables
+    file_contents["LOCAL_FOLDER"] = {}
     with open(os.path.join(project_root, "settings.json"), "w") as file:
         file.write(
             json.dumps(file_contents, indent=4, sort_keys=True)
         )
 
     folder_contents = os.listdir(project_root)
-    files_to_remove = []
     for file in folder_contents:
         if str(file).find("ssh_process_") > -1:
             run_ssh_process = False
 
-    if run_ssh_process:
-        print("Starting ssh_app...")
-        ssh_process = mp.Process(
-            target=ssh_app,
-            args=(project_root,)
-        )
-        ssh_process.start()
-        with open(os.path.join(project_root, "ssh_process_{}.pid".format(ssh_process.pid)), "w") as file:
-            file.write(str(ssh_process.pid))
+    if not run_ssh_process:
+        print("Application instance already exists...")
+        return None
 
-    app.run(debug=True if os.environ.get("DEBUG") == "TRUE" else False)
-        
+    print("Starting ssh_app()...")
+    ssh_process = mp.Process(
+        target=ssh_app,
+        args=(project_root,)
+    )
+    ssh_process.start()
+    ssh_process.join()
+    # with open(os.path.join(project_root, "ssh_process_{}.pid".format(ssh_process.pid)), "w") as file:
+    #     file.write(str(ssh_process.pid))
+    
+
+if __name__ == "__main__":
+
+    main_ssh()
+
+    # app.run(debug=True if os.environ.get("DEBUG") == "TRUE" else False)
